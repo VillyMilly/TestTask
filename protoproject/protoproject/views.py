2a317dfc049bd6aca0c4a00a72c9d6c2e8075122
VillyMilly <96115730+VillyMilly@users.noreply.github.com>
2023-04-25 15:57:29 +0300

    correction files

--- protoproject/protoproject/views.py
+++ protoproject/protoproject/views.py
@@ -1,14 +1,5 @@
-'''from django.http import HttpResponse
-from django.template import loader
-
-
-def start_page(request):
-    template = loader.get_template('default.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)*/'''
-
 from django.shortcuts import render
+
 
 def start_page(request):
     return render(request, 'default.html')
