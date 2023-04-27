703ff13b3761ac84f1e98b01e7989ff2037d1680
VillyMilly <96115730+VillyMilly@users.noreply.github.com>
2023-04-27 23:33:05 +0300

    create model

---
+++ protoproject/protomodels/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProtomodelsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'protomodels'
--- protoproject/protoproject/views.py
+++ protoproject/protoproject/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from protomodels.models import Data
 
 
 def start_page(request):
+    if request.method == "GET":  # suitability check request
+        if request.GET.__contains__('data'):  # checking for the presence of a parameter 'data'
+            a = Data.objects.create(data=request.GET.__getitem__('data'))
     return render(request, 'default.html')
